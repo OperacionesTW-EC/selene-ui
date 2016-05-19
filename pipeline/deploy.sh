752c0ce9328b6ac0c768a9754a90e0c1c1b851e8
valentinsvt <valentinsvt@hotmail.com>
2016-05-19 16:05:56 -0500

    #0 - with @sauljabin - changin deploy file try#4

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,8 +1,12 @@
 echo 'Borrando versiones anteriores'
 rm -rf /var/www/html/selene
+ls -al /var/www/html
 echo 'Descomprimiendo'
 tar  -xvf /var/www/html/selene_ui.tar -C /var/www/html/
+ls -al /var/www/html
 echo 'Desplegando'
 mv /var/www/html/dist /var/www/html/selene
+ls -al /var/www/html
 echo 'Borrando archivo tar'
 rm -rf /var/www/html/selene_ui.tar
+ls -al /var/www/html
