f151d79c65aaf5993f838b510e589fbf70e075ff
juanmnl <juanmnlcornejo@gmail.com>
2016-05-23 10:16:17 -0500

    #51 - adding assign button and primary btn to styles

--- src/components/DeviceList.js
+++ src/components/DeviceList.js
@@ -33,6 +33,7 @@ export default class DeviceList extends React.Component {
             <div className="panel-heading">
                 <div className="row">
                     <div className="col-md-12 text-right">
+                        <Link to='/assign_device' className='btn btn-sm btn-primary btn-create'>Asignar Dispositivo</Link>
                         <Link to='/device_form' className='btn btn-sm btn-secondary btn-create'>Registrar Dispositivo</Link>
                     </div>
                 </div>
