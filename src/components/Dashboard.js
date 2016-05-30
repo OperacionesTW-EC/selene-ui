0e33a9d507497bf9be4d8dced9909036586e2df9
juanmnl <juanmnlcornejo@gmail.com>
2016-05-30 10:17:37 -0500

    #0 - Refactoring css, adding media-queries to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Icon from './helpers/Icon';
 
 const Dashboard = () => {
     return (
-        <section className="container dashboard">
-            <article className="card">
+        <section className='container dashboard'>
+            <article className='card'>
                 <Link to='/assign_device'>
-                    <div className="description">
-                        <div className="icon fa fa-user-plus icon"></div>
-                        <div className="text">Asignar</div>
+                    <div className='description'>
+                        <Icon icon='user-plus icon' />
+                        <div className='text'>Asignar</div>
                     </div>
                 </Link>
             </article>
-            <article className="card">
-                <Link to='/device_form' className="card">
-                    <div className="description">
-                        <div className="fa fa-plus-square-o icon"></div>
-                        <div className="text">Registrar Dispositivo</div>
+            <article className='card'>
+                <Link to='/device_form'>
+                    <div className='description'>
+                        <Icon icon='plus-square-o icon' />
+                        <div className='text'>Registrar Dispositivo</div>
                     </div>
                 </Link>
             </article>
-            <article className="card">
-                <Link to='/device_list' className="card">
-                    <div className="description">
-                        <div className="fa fa-desktop icon"></div>
-                        <div className="text">Dispositivos</div>
+            <article className='card'>
+                <Link to='/device_list'>
+                    <div className='description'>
+                        <Icon icon='desktop icon' />
+                        <div className='text'>Dispositivos</div>
                     </div>
                 </Link>
             </article>
-            <article className="card">
-                <Link to='/assigned_device_list' className="card">
-                    <div className="description">
-                        <div className="fa fa-th-list icon"></div>
-                        <div className="text">Dispositivos Asignados</div>
+            <article className='card'>
+                <Link to='/assigned_device_list'>
+                    <div className='description'>
+                        <Icon icon='th-list icon' />
+                        <div className='text'>Dispositivos Asignados</div>
                     </div>
                 </Link>
             </article>
-            
         </section>
     )
 }
