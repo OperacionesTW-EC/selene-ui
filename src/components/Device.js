a317a7053e66054f5f90344db6f5afa906eb3d79
Saúl Piña <sauljabin@gmail.com>
2016-06-01 12:38:27 -0500

    #21 - With @valentinsvt - rendering device component

--- src/components/Device.js
+++ src/components/Device.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import $ from 'jquery';
 import Constants from './../config/Constants';
+import FormRow from './helpers/FormRow';
+import PageTitle from './layout/PageTitle';
+import DateHelper from './helpers/DateHelper';
+import Icon from './helpers/Icon';
 
 export default class Device extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {device :{}}
+        this.state = {device :{}};
+        this.renderDeviceInfo = this.renderDeviceInfo.bind(this);
+        this.renderDeviceStatusSelect = this.renderDeviceStatusSelect.bind(this);
+        this.handleSaveClick = this.handleSaveClick.bind(this);
     }
 
     componentDidMount(){
@@ -21,9 +28,71 @@ export default class Device extends React.Component {
         });
     }
 
-    render(){
+    render() {
         return(
-            <div>Hello!!!</div>
+            <div>
+                <PageTitle content="Dispositivo" />
+                <div className="container">
+                    <section className="form-card paper white medium">
+                        {this.renderDeviceInfo()}
+                    </section>
+                </div>
+            </div>
+
         )
+    }
+
+    renderDeviceInfo(){
+        return(
+            <div>
+                <FormRow label="Código:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.state.device.full_code}
+                </FormRow>
+                <FormRow label="Marca:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.state.device.device_brand_name}
+                </FormRow>
+                <FormRow label="Tipo:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.state.device.device_type_name}
+                </FormRow>
+                <FormRow label="Modelo:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.state.device.model}
+                </FormRow>
+                <FormRow label="Serial:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.state.device.serial_number}
+                </FormRow>
+                <FormRow label="Fecha de compra:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {DateHelper(this.state.device.purchase_date)}
+                </FormRow>
+                <FormRow label="Fecha de asignación:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {DateHelper(this.state.device.first_assignment_date)}
+                </FormRow>
+                <FormRow label="Fecha de finalización:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {DateHelper(this.state.device.end_date)}
+                </FormRow>
+                <FormRow label="Estado actual:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.state.device.device_status_name}
+                </FormRow>
+                <FormRow label="Cambiar estado:" labelColumnClass="col-md-4" fieldColumnClass="col-md-8">
+                    {this.renderDeviceStatusSelect()}
+                </FormRow>
+                <FormRow>
+                    <a onClick={this.handleSaveClick} id="save" className="btn btn-secondary btn-block">
+                        <Icon icon="save"/> Guardar
+                    </a>
+                </FormRow>
+            </div>
+        )
+    }
+
+    renderDeviceStatusSelect(){
+        return (
+            <select className='form-control' name='new_device_status'>
+                <option>Seleccione...</option>
+            </select>
+        )
+    }
+
+    handleSaveClick() {
+
     }
 }
--- src/spec/components/Device.spec.js
+++ src/spec/components/Device.spec.js
@@ -22,32 +22,79 @@ describe('Device Component', () => {
                 }
             }
         });
-        device = {id:1, full_code: 'TWAL001'};
-        component = mount(<Device params={{id: 1}}/>);
+        sandbox.spy(Device.prototype, "handleSaveClick");
     });
 
     afterEach(function () {
         sandbox.restore();
     });
 
+    describe('Load data', () => {
 
-    it('should load data from backend', () => {
-        expect(component.state('device')).toEqual(device)
-    });
+      it('should load data from backend', () => {
+          device = {id:1, full_code: 'TWAL001'};
+          component = mount(<Device params={{id: 1}}/>);
+          expect(component.state('device')).toEqual(device)
+      });
+
+    })
 
     describe('Render', () => {
 
-        xit('should show device info', () => {
+        it('should show device info', () => {
+            device={"id":1,
+              "device_type_name":"Adaptadores",
+              "full_code":"TWEA0004",
+              "device_brand_name":"Apple",
+              "ownership":"TW",
+              "serial_number":"",
+              "model":"Adaptador VDI",
+              "device_status_name": "Disponible"
+            };
+
+            component = mount(<Device params={{id: 1}}/>);
             let html = component.find('.form-card').node.innerHTML;
             expect(html).toContain(device.full_code);
             expect(html).toContain(device.device_brand_name);
             expect(html).toContain(device.device_type_name);
             expect(html).toContain(device.model);
             expect(html).toContain(device.serial_number);
-            expect(html).toContain(device.purchase_date);
-            expect(html).toContain(device.first_assignment_date);
-            expect(html).toContain(device.end_date);
+            expect(html).toContain(device.device_status_name);
         });
 
+        it('should show formatted purchase, first assignment and end dates', () => {
+            device = {id: 1,
+              "purchase_date":"2016-05-01T15:02:21.960017Z",
+              "first_assignment_date":"2016-05-31T15:02:21.960017Z",
+              "end_date":"2019-05-31T15:02:21.960017Z"
+            };
+
+            component = mount(<Device params={{id: 1}}/>);
+            let html = component.find('.form-card').node.innerHTML;
+            expect(html).toContain('05-01-2016');
+            expect(html).toContain('05-31-2016');
+            expect(html).toContain('05-31-2019');
+        });
+
+        it('should render the new device status field', () => {
+            component = mount(<Device params={{id: 1}}/>);
+    			  expect(component.find('[name="new_device_status"]').length).toBe(1);
+    		});
+
+        it('should render save button', () => {
+    			expect(component.find('#save').length).toBe(1);
+    		});
+
     });
+
+    describe('Events', ()=>{
+
+			it('should invoke handleSaveClick when clicked', () => {
+        component = mount(<Device params={{id: 1}}/>);
+				component.find("#save").simulate('click');
+				expect(Device.prototype.handleSaveClick.calledOnce).toEqual(true);
+			});
+
+    });
+    
 });
