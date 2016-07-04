cb94ec8c6fd87e73821394191605e71ed1f7705c
vrokida <vrokida@gmail.com>
2016-07-04 17:49:29 -0500

    #26 with @pamelipluas Adding test when filter device options change

--- src/spec/components/DeviceList.spec.js
+++ src/spec/components/DeviceList.spec.js
@@ -5,7 +5,9 @@ import expect from 'expect';
 import DeviceList from './../../components/DeviceList'
 import TestUtils from 'react-addons-test-utils';
 import stubRouterContext from './../stubRouterContext';
-import Devices from './../../components/Devices'
+import Devices from './../../components/Devices';
+import { mount } from 'enzyme';
+import Sinon from 'sinon';
 
 describe('DeviceList Component', () => {
     let renderResult;
@@ -28,6 +30,7 @@ describe('DeviceList Component', () => {
 
          it('should render a select list', () => {
             var deviceComponent = TestUtils.findRenderedDOMComponentWithTag(renderResult, 'select');
+
             expect(deviceComponent!=null).toBeTruthy()
         });
 
@@ -62,6 +65,35 @@ describe('DeviceList Component', () => {
             var message = TestUtils.scryRenderedDOMComponentsWithClass(renderResult, 'success-message');
             expect(message.length).toBe(0);
         });
+    });
+
+    describe('device status filter', () => {
+      let select_status_device;
+      let devices;
+      let sandbox;
+      let component;
+
+      beforeEach(() => {
+        let props =  {location:{query:{}}};
+        sandbox = Sinon.sandbox.create();
+        sandbox.spy(DeviceList.prototype,"handleChangeStatus");
+        component = mount(<DeviceList {...props}/>);
+        select_status_device = component.find("[name='device_status']");
+      });
+
+      afterEach(function (){
+        sandbox.restore();
+      });
+
+      it('should call handleChangeStatus when filter change', () => {
+        select_status_device.simulate('change');
+        expect(DeviceList.prototype.handleChangeStatus.calledOnce).toEqual(true);
+      });
+
+      it('should change device_status when filter change', () => {
+        select_status_device.simulate('change', {target : {name:'device_status', value: 'Asignado'}});
+        expect(component.state()['filters']['device_status']).toEqual('Asignado');
+      });
     });
 
 });
