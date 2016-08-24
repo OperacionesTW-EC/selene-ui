0ff4c5cd6cfde71885b00b9d29bdcc6d2653a1c8
pamelipluas <pamelapluas@hotmail.com>
2016-08-24 10:44:58 -0500

    #00 Added ControlAssetDevice

---
+++ src/components/helpers/ControlAssetDevice.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ControlAssetDevice = (props)=> {
+    let rowClass = props.rowClass || 'margin';
+    let fieldColumnClass = props.fieldColumnClass || 'col-md-12 ';
+    let buttonSiClass = props.asset ==1 ? "selected" : ""
+    let buttonNoClass = props.asset ==0 ? "selected" : ""
+
+    return(
+
+              <div className={fieldColumnClass}>
+                  <div className="btn-group">
+                      <button value='1' name="asset" onClick={props.handleFormChanges}
+                          className={"btn btn-default asset-chk " + buttonSiClass }> Si </button>
+                      <button value='0' name="asset" onClick={props.handleFormChanges}
+                          className={"btn btn-default asset-chk " + buttonNoClass}> No </button>
+                  </div>
+              </div>
+
+     )
+}
+
+export default ControlAssetDevice;
